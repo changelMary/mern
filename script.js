0c78031adc16d62a3b960d55bc717791164bd623
changelMary <132337217+changelMary@users.noreply.github.com>
2023-05-02 15:57:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/*add=(a,b)=>{
+    sum=a+b
+    if (sum>10){
+        console.log("large")
+    }
+    else{
+        console.log("small")}
+
+    
+
+}
+add(3,9)*/
+/*let num=[1,2,3,4,5,6,7,8,9,10];
+print = () => {
+    i=0
+   do {
+        console.log(num[i]);
+        i++
+    }while(i<num.length)
+
+}
+print()*/
